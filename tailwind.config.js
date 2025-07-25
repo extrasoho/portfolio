ba9e88240a69dc2bf999802e3117669747467113
jupiter221208 <jupiter221208@gmail.com>
2025-07-25 14:48:26 -0400

    apply font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        uxum: ["Uxum", "sans-serif"],
+        sans: ["Uxum", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
